14587abf42f64748976e7190747cd850281b6aa0
Emory <eehmshaw@gmail.com>
2025-04-06 20:14:57 -0400

    re-add path-prefix var

--- .eleventy.js
+++ .eleventy.js
@@ -143,6 +143,7 @@ module.exports = async function (eleventyConfig) {
       data: '_data',
       output: 'public',
     },
+    pathPrefix: "/mtl-avc-reseau/",
     templateFormats: ['html', 'md', 'njk','css'],//copy any files with these extensions to _site
     htmlTemplateEngine: 'njk',
     markdownTemplateEngine: 'njk',
