6c3430bcf0cbc82e7fce4cf5a8550cbd771e9323
Emory <eehmshaw@gmail.com>
2025-09-11 19:48:53 -0400

    add rule to elevetycomputed

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -135,6 +135,14 @@ module.exports = function () {
         return data.permalink
       }
     
-    }
+    },
+    date: (data) => {
+      // If date is "git Created" and gitCreated is available, use gitCreated
+      if (data.date === "git Created" && data.gitCreated) {
+        return data.gitCreated;
+      }
+      // Otherwise, use the existing date value
+      return data.date;
+    },
   };
 };
