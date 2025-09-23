136bc8d1adaac8dfc25d36c33d6ff7582525038e
maphouse <maphouse@users.noreply.github.com>
2025-09-23 21:11:39 +0000

    deploy: 1fbbbc6ea21caf5c17582b792f18598a7aee4e6a

--- js/tile-gallery.js
+++ js/tile-gallery.js
@@ -1,33 +1,25 @@
 console.log('tile-gallery JS loaded');
 
-function displayDivOnHover(){
-const div = document.querySelector('.tile-gallery');
+// Remove opacity changes for h1/p, but keep zoom and darken effect on hover
+function zoomAndDarkenOnHover() {
+    const div = document.querySelector('.tile-gallery');
     for (let i = 0; i < div.children.length; i += 1) {
-        div.children[i].addEventListener('mouseover', function() {
-            div.children[i].children[0].children[0].style.opacity = .8;
-        });
-        div.children[i].addEventListener('mouseout', function() {
-            div.children[i].children[0].children[0].style.opacity = 0;
-        });
-    }
-}
-displayDivOnHover();
+        const tile = div.children[i];
+        const bgDiv = tile.querySelector('div:first-child');
 
-function zoomBackgroundOnHover() {
-const div = document.querySelector('.tile-gallery');
-    for (let i = 0; i < div.children.length; i += 1) {
-        div.children[i].addEventListener('mouseover', function() {
-            //div.children[i].children[0].style.backgroundSize = '120%';
-            div.children[i].querySelector('div:first-child').style.transform = 'scale(1.025)';
-            //div.children[i].children[0].style.zIndex = '1';
+        tile.addEventListener('mouseover', function() {
+            // Zoom effect
+            bgDiv.style.transform = 'scale(1.025)';
+            // Darken effect
+            bgDiv.style.filter = 'brightness(0.7)';
         });
-        div.children[i].addEventListener('mouseout', function() {
-            //div.children[i].children[0].style.backgroundSize = 'cover';
-            div.children[i].querySelector('div:first-child').style.transform = 'scale(1.0)';
-            //div.children[i].children[0].style.zIndex = '0';
+
+        tile.addEventListener('mouseout', function() {
+            // Reset zoom
+            bgDiv.style.transform = 'scale(1.0)';
+            // Reset darken
+            bgDiv.style.filter = 'brightness(1)';
         });
     }
 }
-zoomBackgroundOnHover();
-//var container = document.querySelector('.container');
-//var text = document.querySelector('#truncated-text');
+zoomAndDarkenOnHover();
